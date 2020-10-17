8f92c397a455fe506bd053e8dccb138cdce6c202
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-10-17 14:15:20 -0600

    auction
    adding uuid
    iam role

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,17 +1,30 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+
 /**
  * @param {object} event: Object that contains all information (params, query) about the event (http)
  * @param {object} context: Object that contains additonal information about the event (http)
  */
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
   const { title } = JSON.parse(event.body);
   const now = new Date();
 
   const auction = {
+    id: uuid(),
     title,
     status: 'OPEN',
     createdAt: now.toISOString(),
   };
+
+  await dynamodb
+    .put({
+      TableName: 'AuctionTable',
+      Item: auction,
+    })
+    .promise();
 
   return {
     statusCode: 201,
