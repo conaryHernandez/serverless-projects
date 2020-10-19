a1a5a528dc52795bd50b4035c0d6f9396c7e54ba
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-10-19 17:13:41 -0600

    auction
    serverless config

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -21,7 +21,7 @@ async function createAuction(event, context) {
 
   await dynamodb
     .put({
-      TableName: 'AuctionTable',
+      TableName: process.env.AUCTIONS_TABLE_NAME,
       Item: auction,
     })
     .promise();
